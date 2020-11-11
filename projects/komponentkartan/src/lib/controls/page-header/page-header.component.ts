3af22eb19c01b91e69e5e2e4c05509f1828b8792
Caroline Bornsjö <carbo15@vgregion.se>
2020-11-11 15:23:19 +0100

    centrerat page och stabiliserat header

--- projects/komponentkartan/src/lib/controls/page-header/page-header.component.ts
+++ projects/komponentkartan/src/lib/controls/page-header/page-header.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, ViewChild, AfterViewChecked, ElementRef, HostListener } from '@angular/core';
+import { Component, Input, ViewChild, AfterViewChecked, ElementRef } from '@angular/core';
 import { PageHeaderHeightService } from '../../services/page-header-height.service';
 
 @Component({
@@ -12,14 +12,12 @@ export class PageHeaderComponent implements AfterViewChecked {
   height = 0;
   private previousHeight = 0;
 
-  visibleState = false;
-
   constructor(private pageHeaderHeightService: PageHeaderHeightService) {}
 
   ngAfterViewChecked() {
     this.setHeight();
-
   }
+
 
   setHeight() {
     this.height = this.pageHeader.nativeElement.offsetTop
